8628c74988d8221353cbb250422f0cb7f9ec5637
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2026-01-16 19:16:59 +0100

    feat: test PUT /driver/workdays

--- api/tests/workday.rs
+++ api/tests/workday.rs
@@ -461,3 +461,163 @@ async fn test_create_workday_duplicate_garbage(ctx: &mut context::TestContext) {
     let body: ErrorBody = res.json();
     assert_eq!(body.error_code, "WORKDAY_ALREADY_EXISTS");
 }
+
+#[test_context(context::TestContext)]
+#[tokio::test]
+async fn test_update_workday_unauthorized(ctx: &mut context::TestContext) {
+    let res = ctx
+        .unauthenticated_router
+        .put("/driver/workdays")
+        .json(&json!({
+            "date": "2026-01-01",
+            "start_time": "08:00:00",
+            "end_time": null,
+            "rest_time": "00:00:00",
+            "overnight_rest": false
+        }))
+        .await;
+
+    res.assert_status(StatusCode::UNAUTHORIZED);
+
+    let body: ErrorBody = res.json();
+    assert_eq!(body.error_code, "UNAUTHORIZED");
+}
+
+#[test_context(context::TestContext)]
+#[tokio::test]
+async fn test_update_workday_success(ctx: &mut context::TestContext) {
+    let res = ctx
+        .authenticated_router
+        .put("/driver/workdays")
+        .json(&json!({
+            "date": "2027-01-01",
+            "start_time": "08:00:00",
+            "end_time": null,
+            "rest_time": "00:00:00",
+            "overnight_rest": false
+        }))
+        .await;
+
+    res.assert_status(StatusCode::OK);
+
+    let body: Workday = res.json();
+
+    let expected_workday = Workday {
+        date: chrono::NaiveDate::from_ymd_opt(2027, 1, 1).unwrap(),
+        start_time: chrono::NaiveTime::from_hms_opt(8, 0, 0).unwrap(),
+        end_time: None,
+        rest_time: chrono::NaiveTime::from_hms_opt(0, 0, 0).unwrap(),
+        overnight_rest: false,
+    };
+    verify_workday_content(body, expected_workday);
+}
+
+#[test_context(context::TestContext)]
+#[tokio::test]
+async fn test_update_workday_not_found(ctx: &mut context::TestContext) {
+    let res = ctx
+        .authenticated_router
+        .put("/driver/workdays")
+        .json(&json!({
+            "date": "2028-01-01",
+            "start_time": "08:00:00",
+            "end_time": null,
+            "rest_time": "00:00:00",
+            "overnight_rest": false
+        }))
+        .await;
+
+    res.assert_status(StatusCode::NOT_FOUND);
+
+    let body: ErrorBody = res.json();
+    assert_eq!(body.error_code, "WORKDAY_NOT_FOUND");
+}
+
+#[test_context(context::TestContext)]
+#[tokio::test]
+async fn test_update_workday_wrong_body(ctx: &mut context::TestContext) {
+    let res1 = ctx
+        .authenticated_router
+        .put("/driver/workdays")
+        .json(&json!({
+            "date": "2026-01",
+            "start_time": "08:00:00",
+            "end_time": null,
+            "rest_time": "00:00:00",
+            "overnight_rest": false
+        }))
+        .await;
+
+    res1.assert_status(StatusCode::BAD_REQUEST);
+
+    let body1: ErrorBody = res1.json();
+    assert_eq!(body1.error_code, "MISSING_ATTRIBUTE");
+
+    let res2 = ctx
+        .authenticated_router
+        .put("/driver/workdays")
+        .json(&json!({
+            "date": "2026-01-01",
+            "start_time": "08:61",
+            "end_time": null,
+            "rest_time": "00:00:00",
+            "overnight_rest": false
+        }))
+        .await;
+
+    res2.assert_status(StatusCode::BAD_REQUEST);
+
+    let body2: ErrorBody = res2.json();
+    assert_eq!(body2.error_code, "MISSING_ATTRIBUTE");
+
+    let res3 = ctx
+        .authenticated_router
+        .put("/driver/workdays")
+        .json(&json!({
+            "date": null,
+            "start_time": "08:00:00",
+            "end_time": null,
+            "rest_time": "00:00:00",
+            "overnight_rest": false
+        }))
+        .await;
+
+    res3.assert_status(StatusCode::BAD_REQUEST);
+
+    let body3: ErrorBody = res3.json();
+    assert_eq!(body3.error_code, "MISSING_ATTRIBUTE");
+
+    let res4 = ctx
+        .authenticated_router
+        .put("/driver/workdays")
+        .json(&json!({
+            "date": "2026-01-01",
+            "start_time": null,
+            "end_time": null,
+            "rest_time": "00:00:00",
+            "overnight_rest": false
+        }))
+        .await;
+
+    res4.assert_status(StatusCode::BAD_REQUEST);
+
+    let body4: ErrorBody = res4.json();
+    assert_eq!(body4.error_code, "MISSING_ATTRIBUTE");
+
+    let res5 = ctx
+        .authenticated_router
+        .put("/driver/workdays")
+        .json(&json!({
+            "date": "2026-01-01",
+            "start_time": "08:00:00",
+            "end_time": null,
+            "rest_time": null,
+            "overnight_rest": false
+        }))
+        .await;
+
+    res5.assert_status(StatusCode::BAD_REQUEST);
+
+    let body5: ErrorBody = res5.json();
+    assert_eq!(body5.error_code, "MISSING_ATTRIBUTE");
+}
