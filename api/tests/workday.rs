fcdac5278333561f4f3a0350ac2f936d0e990125
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2026-01-17 20:40:38 +0100

    fix: add test

--- api/tests/workday.rs
+++ api/tests/workday.rs
@@ -760,13 +760,30 @@ async fn test_get_all_workday_garbage_success(ctx: &mut context::TestContext) {
     res.assert_status(StatusCode::OK);
 
     let body: Vec<WorkdayGarbage> = res.json();
-    assert_eq!(body.len(), 1, "there should be exactly one workday garbage");
+    assert_eq!(body.len(), 2, "there should be exactly two workday garbage");
     assert_eq!(
         body[0].workday_date,
-        chrono::NaiveDate::from_ymd_opt(2026, 1, 15).unwrap()
+        chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap()
     );
     assert_eq!(
         body[0].created_at,
+        chrono::NaiveDateTime::new(
+            chrono::NaiveDate::from_ymd_opt(2024, 1, 1).unwrap(),
+            chrono::NaiveTime::from_hms_opt(8, 45, 0).unwrap()
+        )
+        .and_utc()
+    );
+    assert_eq!(
+        body[0].scheduled_deletion_date,
+        chrono::NaiveDate::from_ymd_opt(2024, 2, 1).unwrap()
+    );
+
+    assert_eq!(
+        body[1].workday_date,
+        chrono::NaiveDate::from_ymd_opt(2026, 1, 15).unwrap()
+    );
+    assert_eq!(
+        body[1].created_at,
         chrono::NaiveDateTime::new(
             chrono::NaiveDate::from_ymd_opt(2026, 2, 10).unwrap(),
             chrono::NaiveTime::from_hms_opt(11, 30, 0).unwrap()
@@ -774,7 +791,7 @@ async fn test_get_all_workday_garbage_success(ctx: &mut context::TestContext) {
         .and_utc()
     );
     assert_eq!(
-        body[0].scheduled_deletion_date,
+        body[1].scheduled_deletion_date,
         chrono::NaiveDate::from_ymd_opt(2026, 3, 11).unwrap()
     );
 }
