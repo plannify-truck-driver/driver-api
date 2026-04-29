04d674b02ef94768aa364c28c8a76f0a6b24d875
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2026-04-29 14:36:39 +0200

    fix: cargo fmt

--- api/src/http/formating/routes.rs
+++ api/src/http/formating/routes.rs
@@ -1,6 +1,13 @@
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-use crate::{AppState, http::formating::handlers::{__path_get_email_formating, __path_get_first_name_formating, __path_get_last_name_formating, get_first_name_formating, get_last_name_formating, get_email_formating}};
+use crate::{
+    AppState,
+    http::formating::handlers::{
+        __path_get_email_formating, __path_get_first_name_formating,
+        __path_get_last_name_formating, get_email_formating, get_first_name_formating,
+        get_last_name_formating,
+    },
+};
 
 pub fn formating_routes() -> OpenApiRouter<AppState> {
     OpenApiRouter::new()
