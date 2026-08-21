5cfc9d267f44f56c304db60c14e022628f769506
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2026-08-21 09:45:38 +0200

    refactor: change logging level from error to info for token decoding failures

--- api/src/http/common/middleware/auth/entities.rs
+++ api/src/http/common/middleware/auth/entities.rs
@@ -136,7 +136,7 @@ impl TokenValidator for AuthValidator {
             &Validation::new(Algorithm::HS256),
         )
         .map_err(|e| {
-            error!("Failed to decode token: {:?}", e);
+            info!("Failed to decode token: {:?}", e);
             ApiError::Unauthorized {
                 error_code: "UNAUTHORIZED".to_string(),
             }
@@ -168,7 +168,7 @@ impl TokenValidator for AuthValidator {
             &Validation::new(Algorithm::HS256),
         )
         .map_err(|e| {
-            error!("Failed to decode refresh token: {:?}", e);
+            info!("Failed to decode refresh token: {:?}", e);
             ApiError::Unauthorized {
                 error_code: "UNAUTHORIZED".to_string(),
             }
